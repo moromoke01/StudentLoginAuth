e26c4851a9fe9e7050be46f3932ee3596d6fdf73
moromoke01 <bellomoromoke2019@gmail.com>
2022-12-09 16:08:42 +0100

    updated App for Login functionality

--- App.js
+++ App.js
@@ -4,7 +4,11 @@ const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
 app.use(cors());
-const bcryptjs = require('bcryptjs')
+const bcrypt = require('bcryptjs')
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "tyihwkggfsujg73987530205()kejfhggt5r9709rehdvjvsdvkjvgfddhglkj9809jklreiuorn8?[]]rstyudj"
+
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -17,42 +21,33 @@ mongoose.connect(mongoUrl, {
 }).then(() =>{console.log("connected to database")})
 .catch(e => console.log(e));
 
+
+
 app.listen(5000, ()=>{
     console.log('server is running')
 })
-//sample
-// app.post("/post", async(req, res) =>{
-//     console.log(req.body)
-//     const { data } =req.body;
 
-//     try{
-//         if(data =="janet"){
-//         res.send({status: "ok"});
-//         }else{
-//             res.send({ status: "User not found"})
-//         }
-//     }catch(error){
-//         res.send({ status: "something went wrong, try again"})
-//       }
-// });
 //getting data that enter into schema
 require("./userDetails");
 const StudentUser = mongoose.model("StudentUserInfo");
 
 //register API to register any user
 app.post("/register", async(req, res) =>{
-    const { uname, email,studentClass,password} = req.body;
+    const { uname, email,studentClass,password } = req.body;
     console.log(req.body)
 
     //to encrypt password
-    const encryptedPassword = await bcryptjs.hash(password, 10);
+    const encryptedPassword = await bcrypt.hash(password, 10);
 
     try{
         //to avoid duplication of user record
-        const oldUser = await StudentUser.findOne({ email });
-        if (oldUser){
+        const check = await StudentUser.findOne({ email });
+        if (check){
             return res.send({ error: "user already exist"})
         }
+        // else{
+        //     return res.send({ status:"ok", message:"Successfully Registered"})
+        // }
        await StudentUser.create({
         uname,
         email,
@@ -68,4 +63,43 @@ app.post("/register", async(req, res) =>{
            })
     }
 })
+//route for login
+app.post("/login-user", async (req, res)=>{
+    const {email,password} = req.body;
+    const user = await StudentUser.findOne({email});
+    if(!user){
+    return res.json({error: "User not found"})    
+}
+
+
+if(await bcrypt.compare(password,user.password)){
+    const token = jwt.sign({email:user.email}, JWT_SECRET)
+    if(res.status(201)){
+        return res.json({
+            status: "ok",
+            data: token
+        })
+    }
+        else{
+            return res.json({error: "error"});
+        }
+    }
+    res.json({status:"error", error:"Invalid password"});
+});
+
+    app.post("/userData", async(req, res)=>{
+        const { token } = req.body;
+        try{
+            const user = jwt.verify(token, JWT_SECRET);
+            const useremail =user.email;
+            StudentUser.findOne({email: useremail})
+            .then((data) =>{
+                res.send({status: "ok", data: data});
+            })
+            .catch((error)=>{
+                res.send({status: "error", data: error})
+            });
+    }catch(error){}
+});
+    
 
